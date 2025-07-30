0f7582a34649f641881f17d23254f28e3a56f575
Michaelninder <fabianeternis@gmail.com>
2025-07-30 14:15:05 +0200

    navbar UPGRADE

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,20 +1,33 @@
 @php
 $links = [
-    'pages.lander' => 'lander',
-    'test' => 'test',
+    [
+        'route' => 'pages.lander',
+        'name' => 'lander',
+        'icon' => 'bi bi-house-door-fill',
+    ],
+    [
+        'route' => 'test',
+        'name' => 'test',
+        'icon' => 'bi bi-joystick',
+    ]
 ];
 @endphp
 
 <div class="navbar">
     <ul>
         @if ($links)
-        @foreach ($links as $route => $name)
-        <li class="nav-item{{ $route === Route::currentRouteName() ? ' active' : '' }}"><a href="{{ route($route) }}" class="nav-link{{ $route === Route::currentRouteName() ? ' active' : '' }}">{{ __('nav.'.$name) }}</a></li>
+        @foreach ($links as $link)
+        <li class="nav-item{{ $link['route'] === Route::currentRouteName() ? ' active' : '' }}">
+            <a href="{{ route($link['route']) }}" class="nav-link{{ $link['route'] === Route::currentRouteName() ? ' active' : '' }}">
+                @if($link['icon'])
+                    <i class="nav-icon {{ $link['icon'] }}"></i>
+                @endif
+                <span class="nav-text">{{ __('nav.'.$link['name']) }}</span>
+            </a>
+        </li>
         @endforeach
         @else
         <div class="nav-error">No Nav-Links avaible</div>
         @endif
     </ul>
 </div>
-
-
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title', 'Showreel') - fabiantrernis.dev</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="{{ asset('css/navbar.css') }}">
 </head>
 <body>
